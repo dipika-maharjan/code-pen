8a216bad413aee148935a2d00509ea1b035a89ad
Dipika <maharjandipika49@gmail.com>
2025-07-07 20:59:14 +0545

    open close btn added

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror';
 import {material} from '@uiw/codemirror-theme-material';
 import {html} from '@codemirror/lang-html';
 import {css} from '@codemirror/lang-css';
 import {javascript} from '@codemirror/lang-javascript';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Editor({displayName, language, value, onChange}) { //displayName is the name of editor, language is the language of the editor, value, onChange passed as a prop
+
+  const [open, setOpen] = useState(true); //for open button
+
     function handleChange(value, viewUpdate){ //value is current value of editor, viewUpdate is the current view of editor
         onChange(value); //this function is called when value of editor changes based on user input
     }
-    return (
-      <div className="editor-container">
+    return (                                                          //collapsed=minimizes the editor
+      <div className={`editor-container ${open ? '' : 'collapsed'}`}>  
         <div className="editor-title">
             {displayName}                 
-            <button>O/C</button>     
+            <button 
+            type='button'
+            className='expand-collapse-btn' 
+            onClick={() => setOpen(prevOpen => ! prevOpen)}> 
+              <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt}/>
+              </button>     
         </div>
 
         <CodeMirror 
